7d55e5b3f42f82cc685a76a9f9f37b604fed7ed4
Dae Young Kim <dykim12222@gmail.com>
2018-12-15 00:13:24 -0500

    f

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ def main():
             #compute intrinsic bonus
             if args.use_tdm:
                 reward_int = tdm.compute_bonus(obs_old, obs).unsqueeze(1).float()
-                reward += args.beta_int * reward_int
+                reward += float(args.beta_int) * reward_int
                 if (j % args.log_interval == 0) and (step == args.num_steps-1):
                     logger.add_reward_intrinsic(reward_int, (j+1)*args.num_steps*args.num_processes)
 
