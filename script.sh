e1a43d9d4fc394fa5b1101b67a380ae9a8d26a6b
Dae Young Kim <dykim12222@gmail.com>
2018-12-15 15:03:39 -0500

    f

--- script.sh
+++ script.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-#
-# # make 0 first
-# import numpy as np
-# betas = 10.0**np.arange(-3,3)
-# bfs = ['const', 'sqrt', 'log', 'linear']
-# bnfs = ['linear', 'square', 'log', 'sqrt']
-#
-# for beta in betas:
-#     for bf in bfs:
-#         for bnf in bnfs:
-#             print("srun python main.py --env-name 'LunarLanderContinuous-v2' --use_tdm True --beta_int {} --num_layers 2 --fc_width 300 --opt_lr 1e-4 --beta_schedule {} --bonus_func {}".format(beta, bf, bnf))
-
-# assume i am at ~/code/tmd/12_15_func
-
-
 count=1
-for beta in 0.001 0.01 0.1 1.0 10.0; do
+for beta in 0.001 0.01 0.1 1.0 10.0 100.0; do
   for bf in "const" "sqrt" "log" "linear"; do
     for bnf in "square" "sqrt" "log" "linear"; do
       mkdir $count
@@ -26,8 +11,7 @@ for beta in 0.001 0.01 0.1 1.0 10.0; do
       cp sb.s mlproject
       cd mlproject
       echo "srun python main.py --env-name 'LunarLanderContinuous-v2' --use_tdm True --beta_int ${beta} --num_layers 2 --fc_width 300 --opt_lr 1e-4 --beta_schedule ${bf} --bonus_func ${bnf}" >> sb.s
+      sbatch sb.s
       cd ~/code/tmd/12_15_func
       count = $((count+1))
-    done;
-  done;
-done;
+done; done; done;
