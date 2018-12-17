0ebac767201d86de72d9264948f648c0791eef59
Dae Young Kim <dykim12222@gmail.com>
2018-12-17 14:34:12 -0500

    f

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ for beta in 0.001 0.01 0.1 1.0 10.0 100.0; do
       cd mlproject
       echo "srun python main.py --env-name 'BipedalWalkerHardcore-v2' --use_tdm True --beta_int ${beta} --num_layers 2 --fc_width 300 --opt_lr 1e-4 --beta_schedule ${bf} --bonus_func ${bnf}" >> sb.s
       sbatch sb.s
-      cd /home/kimdy/code/tmd/12_16_bipedalhardcore
+      cd /home/kimdy/code/tmd/12_17_bipedalhardcore
       count=$((count+1))
 done; done; done;
 
